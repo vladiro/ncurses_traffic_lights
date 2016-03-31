0b67dcf81475708970d7baab9632bc280ad4d3e3
Dean Neil Butcher <butchdeanca@gmail.com>
2016-03-30 22:59:13 -0300

    Bigger lights and frame.

--- src/lightsframe.c
+++ src/lightsframe.c
@@ -26,63 +26,71 @@ void DrawSignalInit()
 
 void DrawLightsFrame()
 {
+    unsigned int idx;
+
     attron(COLOR_PAIR(1));
 
     mvprintw(5, LEFT_MARGIN, TOP_BOTTOM);
-    mvprintw(6, LEFT_MARGIN, SIDES);
-    mvprintw(6, RIGHT_MARGIN, SIDES);
-    mvprintw(7, LEFT_MARGIN, SIDES);
-    mvprintw(7, RIGHT_MARGIN, SIDES);
-    mvprintw(8, LEFT_MARGIN, SIDES);
-    mvprintw(8, RIGHT_MARGIN, SIDES);
-    mvprintw(9, LEFT_MARGIN, TOP_BOTTOM);
 
-    mvprintw(10, LEFT_MARGIN, SIDES);
-    mvprintw(10, RIGHT_MARGIN, SIDES);
-    mvprintw(11, LEFT_MARGIN, SIDES);
-    mvprintw(11, RIGHT_MARGIN, SIDES);
-    mvprintw(12, LEFT_MARGIN, SIDES);
-    mvprintw(12, RIGHT_MARGIN, SIDES);
-    mvprintw(13, LEFT_MARGIN, TOP_BOTTOM);
+    for(idx = 6; idx < 15; idx++)
+    {
+        mvprintw(idx, LEFT_MARGIN, SIDES);
+        mvprintw(idx, RIGHT_MARGIN, SIDES);
+    }
 
-    mvprintw(14, LEFT_MARGIN, SIDES);
-    mvprintw(14, RIGHT_MARGIN, SIDES);
-    mvprintw(15, LEFT_MARGIN, SIDES);
-    mvprintw(15, RIGHT_MARGIN, SIDES);
-    mvprintw(16, LEFT_MARGIN, SIDES);
-    mvprintw(16, RIGHT_MARGIN, SIDES);
-    mvprintw(17, LEFT_MARGIN, TOP_BOTTOM);
+    mvprintw(15, LEFT_MARGIN, TOP_BOTTOM);
+
+    for(idx = 16; idx < 25; idx++)
+    {
+        mvprintw(idx, LEFT_MARGIN, SIDES);
+        mvprintw(idx, RIGHT_MARGIN, SIDES);
+    }
+
+    mvprintw(25, LEFT_MARGIN, TOP_BOTTOM);
+
+    for(idx = 26; idx < 35; idx++)
+    {
+        mvprintw(idx, LEFT_MARGIN, SIDES);
+        mvprintw(idx, RIGHT_MARGIN, SIDES);
+    }
+
+    mvprintw(35, LEFT_MARGIN, TOP_BOTTOM);
 
     attroff(COLOR_PAIR(1));
 }
 
 void DrawSignalLights()
 {
-    const char* LIGHTS_SPECULAR = "*****";
+    const char* LIGHTS_SPECULAR = "***************";
+    unsigned int idx;
 
     attron(COLOR_PAIR(2));
 
-    mvprintw(6, LIGHTS_FRAME, LIGHTS_SPECULAR);
-    mvprintw(7, LIGHTS_FRAME, LIGHTS_SPECULAR);
-    mvprintw(8, LIGHTS_FRAME, LIGHTS_SPECULAR);
+    for(idx = 6; idx < 15; idx++)
+    {
+        mvprintw(idx, LIGHTS_FRAME, LIGHTS_SPECULAR);
+    }
 
     attroff(COLOR_PAIR(2));
 
     attron(COLOR_PAIR(1));
 
-    mvprintw(10, LIGHTS_FRAME, LIGHTS_SPECULAR);
-    mvprintw(11, LIGHTS_FRAME, LIGHTS_SPECULAR);
-    mvprintw(12, LIGHTS_FRAME, LIGHTS_SPECULAR);
+    for(idx = 16; idx < 25; idx++)
+    {
+        mvprintw(idx, LIGHTS_FRAME, LIGHTS_SPECULAR);
+    }
 
     attroff(COLOR_PAIR(1));
 
     attron(COLOR_PAIR(4));
 
-    mvprintw(14, LIGHTS_FRAME, LIGHTS_SPECULAR);
-    mvprintw(15, LIGHTS_FRAME, LIGHTS_SPECULAR);
-    mvprintw(16, LIGHTS_FRAME, LIGHTS_SPECULAR);
+    for(idx = 26; idx < 35; idx++)
+    {
+        mvprintw(idx, LIGHTS_FRAME, LIGHTS_SPECULAR);
+    }
 
     attroff(COLOR_PAIR(4));
+
 }
 
 void DrawPedestrianLightsFrame()
