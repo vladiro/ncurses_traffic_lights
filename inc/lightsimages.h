6a23f1f961ea0a119a99d9ce8c62abb3096a541e
Dean Neil Butcher <butchdeanca@gmail.com>
2016-03-31 22:20:36 -0300

    Source files for pedestrian signal images.

---
+++ inc/lightsimages.h
@@ -0,0 +1,15 @@
+#ifndef _SIGNAL_IMAGES_
+#define _SIGNAL_IMAGES_
+
+int Draw0(const unsigned int displacement);
+int Draw1(const unsigned int displacement);
+int Draw2(const unsigned int displacement);
+int Draw3(const unsigned int displacement);
+int Draw4(const unsigned int displacement);
+int Draw5(const unsigned int displacement);
+int Draw6(const unsigned int displacement);
+int Draw7(const unsigned int displacement);
+int Draw8(const unsigned int displacement);
+int Draw9(const unsigned int displacement);
+
+#endif
---
+++ src/lightsimages.c
@@ -0,0 +1,51 @@
+#include <lightsimages.h>
+
+int Draw0(const unsigned int displacement)
+{
+    return 0;
+}
+
+int Draw1(const unsigned int displacement)
+{
+    return 1;
+}
+
+int Draw2(const unsigned int displacement)
+{
+    return 2;
+}
+
+int Draw3(const unsigned int displacement)
+{
+    return 3;
+}
+
+int Draw4(const unsigned int displacement)
+{
+    return 4;
+}
+
+int Draw5(const unsigned int displacement)
+{
+    return 5;
+}
+
+int Draw6(const unsigned int displacement)
+{
+    return 6;
+}
+
+int Draw7(const unsigned int displacement)
+{
+    return 7;
+}
+
+int Draw8(const unsigned int displacement)
+{
+    return 8;
+}
+
+int Draw9(const unsigned int displacement)
+{
+    return 9;
+}
