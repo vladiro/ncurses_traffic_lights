2be5a9c34ca29d436b3986a7dfb33e6456675cde
Dean Neil Butcher <butchdeanca@gmail.com>
2016-04-04 15:56:51 -0300

    Just chanigng my approach after much thought.

--- inc/lightsimages.h
+++ inc/lightsimages.h
@@ -1,15 +1,6 @@
 #ifndef _SIGNAL_IMAGES_
 #define _SIGNAL_IMAGES_
 
-int Draw0(const unsigned int displacement);
-int Draw1(const unsigned int displacement);
-int Draw2(const unsigned int displacement);
-int Draw3(const unsigned int displacement);
-int Draw4(const unsigned int displacement);
-int Draw5(const unsigned int displacement);
-int Draw6(const unsigned int displacement);
-int Draw7(const unsigned int displacement);
-int Draw8(const unsigned int displacement);
-int Draw9(const unsigned int displacement);
+int DrawDigit(const unsigned int DIGIT, const unsigned int XDISP, const unsigned int YDISP);
 
 #endif
--- src/lightsimages.c
+++ src/lightsimages.c
@@ -1,51 +1,16 @@
+#include <commonincs.h>
 #include <lightsimages.h>
 
-int Draw0(const unsigned int displacement)
+static const char* HORIZONTAL_BAR = "oooooo";
+static const char* SIDE_BAR = "o";
+
+int DrawDigit(const unsigned int DIGIT, const unsigned int XDISP, const unsigned int YDISP)
 {
+    attron(COLOR_PAIR(2));
+
+
+
+    attroff(COLOR_PAIR(2));
+
     return 0;
-}
-
-int Draw1(const unsigned int displacement)
-{
-    return 1;
-}
-
-int Draw2(const unsigned int displacement)
-{
-    return 2;
-}
-
-int Draw3(const unsigned int displacement)
-{
-    return 3;
-}
-
-int Draw4(const unsigned int displacement)
-{
-    return 4;
-}
-
-int Draw5(const unsigned int displacement)
-{
-    return 5;
-}
-
-int Draw6(const unsigned int displacement)
-{
-    return 6;
-}
-
-int Draw7(const unsigned int displacement)
-{
-    return 7;
-}
-
-int Draw8(const unsigned int displacement)
-{
-    return 8;
-}
-
-int Draw9(const unsigned int displacement)
-{
-    return 9;
 }
