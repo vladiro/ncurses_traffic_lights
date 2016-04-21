d045f319c86b63229b52060dda6178c7f43c0d37
Dean Neil Butcher <butchdeanca@gmail.com>
2016-04-21 00:47:38 -0300

    Most signal functionality completed.

--- src/lightslogic.c
+++ src/lightslogic.c
@@ -15,46 +15,55 @@ int SystemIsReset()
 
 void SetStatusRed()
 {
-
+    systemStatus |= SHIFTLEFT(2);
 }
 
 void SetStatusAmber()
 {
-
+    systemStatus |= SHIFTLEFT(1);
 }
 
 void SetStatusGreen()
 {
-
+    systemStatus |= SHIFTLEFT(0);
 }
 
 void ClearStatusRed()
 {
-
+    systemStatus &= ~SHIFTLEFT(2);
 }
 
 void ClearStatusAmber()
 {
-
+    systemStatus &= ~SHIFTLEFT(1);
 }
 
 void ClearStatusGreen()
 {
-
+    systemStatus &= ~SHIFTLEFT(0);
 }
 
 int GetStatusRed()
 {
+    if(systemStatus & SHIFTLEFT(2))
+        return 1;
+
     return 0;
 }
 
 int GetStatusAmber()
 {
+    if(systemStatus & SHIFTLEFT(1))
+        return 1;
+
     return 0;
 }
 
 int GetStatusGreen()
 {
+    if(systemStatus & SHIFTLEFT(0))
+        return 1;
+
     return 0;
 }
 
