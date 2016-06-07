36768491907a68b55cc0bc85734abdcb231fc557
Dean Neil Butcher <butchdeanca@gmail.com>
2016-06-06 23:37:43 -0300

    Pedestrian signal now changes although in incorrect order.

--- src/main.c
+++ src/main.c
@@ -55,6 +55,7 @@ int main()
             LightsCycle(testCycle);
             PedestrianLightsCycle(testCycle);
             DrawSignalLights();
+            DrawPedestrianSignal(23, 17);
 
             DrawSignalRefresh();
 
